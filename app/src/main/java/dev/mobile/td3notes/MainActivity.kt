b0606135d6fd9629385b7c34c170935664f42ae0
Nicolas CHUSSEAU <E214406A@ubuntu.u-inf-j-c001-05>
2023-10-13 16:50:35 +0200

    récupéré photo depuis galerie ok, prendre une photo ok, la photo prise ne se memt pas dans la vue directement

--- app/src/main/java/dev/mobile/td3notes/MainActivity.kt
+++ app/src/main/java/dev/mobile/td3notes/MainActivity.kt
@@ -1,20 +1,49 @@
 package dev.mobile.td3notes
 
-
+import android.content.Context
+import androidx.activity.result.ActivityResultLauncher
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import androidx.appcompat.app.AppCompatActivity
+import android.widget.ImageView
+import androidx.activity.result.contract.ActivityResultContracts
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var bouton : Button
+    private lateinit var buttonTakePicture: Button
+    private lateinit var buttonChooseFromGallery: Button
+    private lateinit var imagePreview: ImageView
 
-
+    private lateinit var takePictureLauncher: ActivityResultLauncher<Context>
+    private lateinit var chooseFromGalleryLauncher: ActivityResultLauncher<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        bouton = findViewById(R.id.button)
 
+        buttonTakePicture = findViewById(R.id.button_take_picture)
+        buttonChooseFromGallery = findViewById(R.id.button_choose_from_gallery)
+        imagePreview = findViewById(R.id.image_preview)
+
+        takePictureLauncher = registerForActivityResult(TakePictureContract()) { uri ->
+            if (uri != null) {
+                imagePreview.setImageURI(uri)
+                imagePreview.visibility = ImageView.VISIBLE
+            }
+        }
+
+        chooseFromGalleryLauncher = registerForActivityResult(ActivityResultContracts.GetContent()) { uri ->
+            if (uri != null) {
+                imagePreview.setImageURI(uri)
+                imagePreview.visibility = ImageView.VISIBLE
+            }
+        }
+
+        buttonTakePicture.setOnClickListener {
+            takePictureLauncher.launch(this)
+        }
+
+        buttonChooseFromGallery.setOnClickListener {
+            chooseFromGalleryLauncher.launch("image/*")
+        }
     }
-
 }
