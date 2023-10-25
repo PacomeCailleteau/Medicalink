17697a5abb0f62d8d84db197d17b2758a6b9ecdc
E214406A <nicolas.chusseau@etu.univ-nantes.fr>
2023-10-25 10:42:38 +0200

    Ajout des fragments (pas fini mais commencé). Tout ce qui est fait fonctionne

---
+++ app/src/main/java/dev/mobile/medicalink/AddTraitementsFragment.kt
@@ -0,0 +1,26 @@
+package dev.mobile.medicalink
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+
+
+class AddTraitementsFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_add_traitements, container, false)
+
+
+
+
+        return view
+    }
+
+}
--- app/src/main/java/dev/mobile/medicalink/MainActivity.kt
+++ app/src/main/java/dev/mobile/medicalink/MainActivity.kt
@@ -1,9 +1,9 @@
 package dev.mobile.medicalink
 
-import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.widget.ImageView
 import android.widget.TextView
 
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
         buttonChangerUtilisateur = findViewById(R.id.button_changer_utilisateur)
 
         buttonConnexion.setOnClickListener {
-            val intent = Intent(this, MainTraitementsActivity::class.java)
+            val intent = Intent(this, MainFragment::class.java)
             startActivity(intent)
         }
 
