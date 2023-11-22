ae087b5c236d77287e4f41edc6a2079176c5c5ce
E211556C <lilian.delhommeau@etu.univ-nantes.fr>
2023-11-22 15:33:41 +0100

    Ajout vue Stock "AjoutManuelStock"

---
+++ app/src/main/java/dev/mobile/medicalink/fragments/traitements/AjoutManuelStock.kt
@@ -0,0 +1,83 @@
+package dev.mobile.medicalink.fragments.traitements
+
+import android.os.Build
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.annotation.RequiresApi
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.google.android.material.textfield.TextInputEditText
+import dev.mobile.medicalink.R
+
+class AjoutManuelStock : Fragment() {
+    private lateinit var retour: ImageView
+    private lateinit var suivant : Button
+
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        val view = inflater.inflate(R.layout.fragment_ajout_manuel_stock, container, false)
+
+        if (activity != null) {
+            val navBarre = requireActivity().findViewById<ConstraintLayout>(R.id.fragmentDuBas)
+            navBarre.visibility = View.GONE
+        }
+
+        retour = view.findViewById(R.id.retour_schema_prise2)
+        suivant = view.findViewById(R.id.suivant1)
+
+        val traitement = arguments?.getSerializable("traitement") as Traitement
+        var schema_prise1  = arguments?.getString("schema_prise1")
+        var provenance  = arguments?.getString("provenance")
+        var dureePriseDbt = arguments?.getString("dureePriseDbt")
+        var dureePriseFin = arguments?.getString("dureePriseFin")
+
+
+
+
+
+        suivant.setOnClickListener {
+            val bundle = Bundle()
+            bundle.putSerializable("traitement", Traitement(traitement.nomTraitement, traitement.dosageNb,traitement.dosageUnite,null,traitement.typeComprime,25,false,null,traitement.prises))
+            bundle.putString("schema_prise1", "$schema_prise1")
+            bundle.putString("provenance", "$provenance")
+            bundle.putString("dureePriseDbt", "$dureePriseDbt")
+            bundle.putString("dureePriseFin", "$dureePriseFin")
+            var destinationFragment = AjoutManuelDateSchemaPrise()
+            destinationFragment.arguments = bundle
+            val fragTransaction = parentFragmentManager.beginTransaction()
+            fragTransaction.replace(R.id.FL, destinationFragment)
+            fragTransaction.addToBackStack(null)
+            fragTransaction.commit()
+        }
+
+
+
+        retour.setOnClickListener {
+            //On appelle le parent pour changer de fragment
+            val bundle = Bundle()
+            bundle.putSerializable("traitement", Traitement(traitement.nomTraitement,traitement.dosageNb,traitement.dosageUnite,traitement.dateFinTraitement,traitement.typeComprime,25,false,null,traitement.prises))
+            bundle.putString("schema_prise1", "$schema_prise1")
+            bundle.putString("provenance", "$provenance")
+            bundle.putString("dureePriseDbt", "$dureePriseDbt")
+            bundle.putString("dureePriseFin", "$dureePriseFin")
+            val destinationFragment = AjoutManuelDateSchemaPrise()
+            destinationFragment.arguments = bundle
+            val fragTransaction = parentFragmentManager.beginTransaction()
+            fragTransaction.replace(R.id.FL, destinationFragment)
+
+            fragTransaction.addToBackStack(null)
+            fragTransaction.commit()
+        }
+        return view
+    }
+}
