a2c0de40b6e3b59c8bb2e1f44960643f52235793
E214406A <nicolas.chusseau@etu.univ-nantes.fr>
2023-11-22 14:44:58 +0100

    réparation delete all data lors de l'appui sur le bouton

--- app/src/main/java/dev/mobile/medicalink/fragments/home/ParametreFragement.kt
+++ app/src/main/java/dev/mobile/medicalink/fragments/home/ParametreFragement.kt
@@ -1,12 +1,12 @@
 package dev.mobile.medicalink.fragments.home
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.ImageView
 import dev.mobile.medicalink.R
 import dev.mobile.medicalink.db.local.AppDatabase
 
@@ -31,7 +31,7 @@ class ParametreFragement : Fragment() {
                 //Call tsarBomba function to clear all tables :)
                 //ça va faire boum badaboum
                 db.tsarBomba()
-            }
+            }.start()
         }
 
         return view
