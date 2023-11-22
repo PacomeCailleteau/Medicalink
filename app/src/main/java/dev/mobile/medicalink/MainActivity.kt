fe78f78089b962ee2df2d8fc0d91cce2d1b247d3
E212406B <pacome.cailleteau@etu.univ-nantes.fr>
2023-11-22 08:26:26 +0100

    remove notif imédiate

--- app/src/main/java/dev/mobile/medicalink/MainActivity.kt
+++ app/src/main/java/dev/mobile/medicalink/MainActivity.kt
@@ -25,40 +25,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Dans la méthode `onCreate` de votre MainActivity
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channelId = "channel_id"
-            val channelName = "Nom du canal"
-            val importance = NotificationManager.IMPORTANCE_DEFAULT
-            val channel = NotificationChannel(channelId, channelName, importance)
-
-            val notificationManager =
-                getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            notificationManager.createNotificationChannel(channel)
-
-            val notificationBuilder = NotificationCompat.Builder(this, "channel_id")
-                .setSmallIcon(R.drawable.cloche)
-                .setContentTitle("Notification immédiate")
-                .setContentText("Cette notification est envoyée immédiatement.")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-
-            notificationManager.notify(1, notificationBuilder.build())
-
-            // Dans la méthode `onCreate` de votre MainActivity
-            val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
-            val notificationIntent = Intent(this, NotificationService::class.java)
-            //val pendingIntent = PendingIntent.getBroadcast(this, 0, notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT)
-
-            // Calculer le temps actuel + 1 minutes
-            val currentTimeMillis = System.currentTimeMillis()
-            val oneMinutesMillis = 6000
-            val futureTimeMillis = currentTimeMillis + oneMinutesMillis
-
-            //alarmManager.set(AlarmManager.RTC_WAKEUP, futureTimeMillis, pendingIntent)
-
-        }
-
-
         //masquer la barre de titre
         supportActionBar?.hide()
 
