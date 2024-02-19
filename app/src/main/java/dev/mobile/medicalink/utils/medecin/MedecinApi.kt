350d70fa34004927dd3721670f1622650b887667
NicolasChusseau <nicolas.chusseau@etu.univ-nantes.fr>
2024-02-19 10:25:02 +0100

    fix pb test

--- app/src/main/java/dev/mobile/medicalink/utils/medecin/MedecinApi.kt
+++ app/src/main/java/dev/mobile/medicalink/utils/medecin/MedecinApi.kt
@@ -1,12 +1,10 @@
 package dev.mobile.medicalink.utils.medecin
 
 
-import android.util.Log
 import com.google.gson.Gson
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.Response
-import org.json.JSONObject
 
 /**
  * Classe permettant de gérer les appels à l'API pour les médecins
@@ -20,17 +18,15 @@ class MedecinApi {
             val url = "${baseURL}medecin/$rpps"
             val client = OkHttpClient()
             val request = Request.Builder().url(url).build()
-            println(url)
             val response: Response = client.newCall(request).execute()
-            println(" $rpps")
+
             if (!response.isSuccessful) {
                 return null
             }
 
             val body = response.body?.string()
-            val jsonBody = body?.let { JSONObject(it) }
             val gson = Gson()
-            return gson.fromJson(jsonBody.toString(), Medecin::class.java)
+            return gson.fromJson(body.toString(), Medecin::class.java)
 
         } catch (e: Exception) {
             return null
