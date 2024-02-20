a1b3eece02ed0ce1a1ff45046ecf9538372c7d19
E210826J <mathieu.bergeron@etu.univ-nantes.fr>
2024-02-20 14:49:35 +0100

    code is very qualitative and very pretty again

--- app/src/main/java/dev/mobile/medicalink/tokenization/FullTokenizer.kt
+++ app/src/main/java/dev/mobile/medicalink/tokenization/FullTokenizer.kt
@@ -26,7 +26,7 @@ final class FullTokenizer(
     fun convertTokensToIds(tokens: List<String>): MutableList<Int> {
         val outputIds: MutableList<Int> = ArrayList()
         for (token in tokens) {
-            outputIds.add(dic[token] ?: dic["[UNK]"]!!.toInt())
+            outputIds.add(dic[token] ?: -1)
         }
         return outputIds
     }
