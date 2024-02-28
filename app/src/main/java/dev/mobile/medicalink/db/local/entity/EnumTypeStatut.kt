a7605b4eea1e021b9ee603f75dc5d399aa146d6f
Ad2rien5 <seach.adrien@gmail.com>
2024-02-28 14:23:04 +0100

    Ajout de getStringFromEnum pour EnumTypeStatut.kt + les traductions qui manquaient

--- app/src/main/java/dev/mobile/medicalink/db/local/entity/EnumTypeStatut.kt
+++ app/src/main/java/dev/mobile/medicalink/db/local/entity/EnumTypeStatut.kt
@@ -1,7 +1,20 @@
 package dev.mobile.medicalink.db.local.entity
 
+import android.content.Context
+import dev.mobile.medicalink.R
+
 enum class EnumTypeStatut {
     Medicament,
     Intervalle,
     Spontanee;
+
+    companion object {
+        fun getStringFromEnum(type: EnumTypeStatut, context: Context): String {
+            return when (type) {
+                Medicament -> context.getString(R.string.medicament)
+                Intervalle -> context.getString(R.string.dialog_intervalle_regulier)
+                Spontanee -> context.getString(R.string.mois)
+            }
+        }
+    }
 }
